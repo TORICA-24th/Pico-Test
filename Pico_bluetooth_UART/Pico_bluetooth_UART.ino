a20f73fed81bf99d014600964389b62fec8610f5
Syo-121 <131446710+Syo-121@users.noreply.github.com>
2023-12-01 10:53:09 +0900

    update bluetooth_UART

--- Pico_bluetooth_UART/Pico_bluetooth_UART.ino
+++ Pico_bluetooth_UART/Pico_bluetooth_UART.ino
@@ -4,21 +4,14 @@ void setup()
 {
   Serial.begin(115200);
   SerialBT.begin(115200);
+
+  pinMode(5,OUTPUT);
 }
 
 void loop()
 {
-
-  while(SerialBT)
-  {
-    /*//UARTにデータがあれば読み出して、BluetoothUARTに送信
-    if(Serial.available())
-    {
-      SerialBT.write(Serial.read());
-    }
-    */
-    //BluetoothUARTにデータがあれば読み出して、UARTに送信
-    SerialBT.println("My name is raspberry pi pico");
-    delay(1000);
+  if(SerialBT.available()){
+    digitalWrite(5,HIGH);
+    SerialBT.println("Hello");
   }
 }
